a6fc3825367eceb05b3ad279da836ee32b0ea6c6
Ricardo M Borges <ricardo_mborges@yahoo.com.br>
2026-03-02 18:39:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,9 @@ st.markdown("""
 This tool allows you to:
 
 1. Generate experimental designs
-    2. Run experiments
+2. Go to the Lab
+    2.1. Run experiments
+2.2. Come back here
 3. Upload results
 4. Fit a **quadratic** model (main + interactions + curvature)
 5. Visualize response surfaces (2D contour + 3D surface)
@@ -984,6 +986,7 @@ with tab3:
             real_best[spec["name"]] = coded_to_real_value(cval, spec)
 
         st.write("Best real conditions:", real_best)
+
 
 
 
